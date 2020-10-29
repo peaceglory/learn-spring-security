205366d49167f05173c6a2226c18e49a8324ec2c
Miroslav Manchev <miroslav.manchev@accelya.com>
2020-10-29 17:46:22 +0200

    .

--- oauth-client-start/oauth-client-start--client/src/main/java/com/baeldung/lsso/spring/ClientSecurityConfig.java
+++ oauth-client-start/oauth-client-start--client/src/main/java/com/baeldung/lsso/spring/ClientSecurityConfig.java
@@ -1,8 +1,13 @@
 package com.baeldung.lsso.spring;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @EnableWebSecurity
 public class ClientSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -10,8 +15,28 @@ public class ClientSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {// @formatter:off
         http.authorizeRequests()
-            .antMatchers("/").permitAll()
-            .anyRequest().authenticated();
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                //                .oauth2Client().authorizationCodeGrant().authorizationRequestRepository();
+                .oauth2Login()
+                .and()
+                .logout().logoutSuccessUrl("/");
     }// @formatter:on
 
+    @Bean
+    WebClient webClient(ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+        // To include the access token in the request header.
+        final var clientExchangeFilter =
+                new ServletOAuth2AuthorizedClientExchangeFilterFunction(clientRegistrationRepository,
+                        authorizedClientRepository);
+
+        // All HTTP communication will possess the access token, so we need to make sure this webClient instance is
+        // used only for communication with the resource server.
+        clientExchangeFilter.setDefaultOAuth2AuthorizedClient(true);
+
+        return WebClient.builder().apply(clientExchangeFilter.oauth2Configuration()).build();
+    }
 }
--- oauth-client-start/oauth-client-start--client/src/main/java/com/baeldung/lsso/web/controller/ProjectClientController.java
+++ oauth-client-start/oauth-client-start--client/src/main/java/com/baeldung/lsso/web/controller/ProjectClientController.java
@@ -3,20 +3,39 @@ package com.baeldung.lsso.web.controller;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.baeldung.lsso.web.model.ProjectModel;
 
 @Controller
 public class ProjectClientController {
+    private final WebClient webClient;
+
+    @Value("${resourceserver.api.project.url}")
+    private String resourceServerUri;
+
+    public ProjectClientController(WebClient webClient) {
+        this.webClient = webClient;
+    }
 
     @GetMapping("/projects")
     public String getProjects(Model model) {
-        List<ProjectModel> projects = null;
+        List<ProjectModel> projects = webClient
+                .get()
+                .uri(resourceServerUri)
+                //                .attributes(ServletOAuth2AuthorizedClientExchangeFilterFunction.clientRegistrationId("custom"))
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<ProjectModel>>() {})
+                .block();
+
         model.addAttribute("projects", projects);
         return "projects";
     }
