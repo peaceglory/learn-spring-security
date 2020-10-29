1916f8af4a852c61039e035052ad307688870a8b
Miroslav Manchev <miroslav.manchev@accelya.com>
2020-10-29 19:29:44 +0200

    .

--- resource-server-start/resource-server-start--resource-server/src/main/java/com/baeldung/lsso/spring/ResourceSecurityConfig.java
+++ resource-server-start/resource-server-start--resource-server/src/main/java/com/baeldung/lsso/spring/ResourceSecurityConfig.java
@@ -1,8 +1,23 @@
 package com.baeldung.lsso.spring;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class ResourceSecurityConfig {
+//@EnableWebSecurity
+public class ResourceSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // If this is skipped the default autoconfiguration is used. Check OAuth2ResourceServerAutoConfiguration
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {// @formatter:off
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+    }//@formatter:on
 
 }
