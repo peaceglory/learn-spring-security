a64079d3c579e34ccac0bc666704e98c8d50d9ea
Miroslav Manchev <miroslav.manchev@accelya.com>
2020-11-23 14:23:24 +0200

    .

--- basic-authorization-start/basic-authorization-start--resource-server/src/main/java/com/baeldung/lsso/spring/ResourceSecurityConfig.java
+++ basic-authorization-start/basic-authorization-start--resource-server/src/main/java/com/baeldung/lsso/spring/ResourceSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.baeldung.lsso.spring;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,12 +12,16 @@ public class ResourceSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {// @formatter:off
-        http.authorizeRequests()
-              .anyRequest()
-                .authenticated()
-            .and()
-              .oauth2ResourceServer()
-                .jwt();
+        http
+                .authorizeRequests()
+                    .antMatchers(HttpMethod.GET, "/api/projects/**")
+                    .hasAuthority("SCOPE_read")
+                    .antMatchers(HttpMethod.POST, "/api/projects")
+                    .hasAuthority("SCOPE_write")
+                    .anyRequest()
+                    .authenticated() // TODO What happens if we skip anyRequest().authenticated() and instead make specific URL only authenticated()
+                .and()
+                    .oauth2ResourceServer()
+                    .jwt();
     }//@formatter:on
-
 }
--- basic-authorization-start/basic-authorization-start--resource-server/src/main/java/com/baeldung/lsso/web/controller/ProjectController.java
+++ basic-authorization-start/basic-authorization-start--resource-server/src/main/java/com/baeldung/lsso/web/controller/ProjectController.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,6 +32,7 @@ public class ProjectController {
     }
 
     @GetMapping(value = "/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_email')")
     public ProjectDto findOne(@PathVariable Long id) {
         Project entity = projectService.findById(id)
             .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
