7525dee93c87ee96367a56fb60be77a396897877
Miroslav Manchev <miroslav.manchev@accelya.com>
2020-11-30 10:34:23 +0200

    .

--- accessing-token-authentication-attributes-spel-start/accessing-token-authentication-attributes-spel-start--resource-server/src/main/java/com/baeldung/lsso/spring/ResourceSecurityConfig.java
+++ accessing-token-authentication-attributes-spel-start/accessing-token-authentication-attributes-spel-start--resource-server/src/main/java/com/baeldung/lsso/spring/ResourceSecurityConfig.java
@@ -2,10 +2,12 @@ package com.baeldung.lsso.spring;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
--- accessing-token-authentication-attributes-start/accessing-token-authentication-attributes-start--resource-server/src/main/java/com/baeldung/lsso/web/controller/UserInfoController.java
+++ accessing-token-authentication-attributes-start/accessing-token-authentication-attributes-start--resource-server/src/main/java/com/baeldung/lsso/web/controller/UserInfoController.java
@@ -1,16 +1,27 @@
 package com.baeldung.lsso.web.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.Map;
 
 @RestController
 public class UserInfoController {
 
     @GetMapping("/user/info")
-    public Map<String, Object> getUserInfo() {
+    public Map<String, Object> getUserInfo(Authentication authentication) {
+        final var jwt = (Jwt) authentication.getPrincipal();
 
-        return null;
+        return Collections.singletonMap("token", jwt);
+    }
+
+    @GetMapping("/user/info/direct")
+    public Map<String, Object> getUserInfoDirect(@AuthenticationPrincipal Jwt principal) {
+        final var username = principal.getClaims().get("preferred_username");
+        return Collections.singletonMap("username", username);
     }
 }
