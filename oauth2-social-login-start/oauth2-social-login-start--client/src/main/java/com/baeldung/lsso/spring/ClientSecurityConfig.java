68d6d753d42471c8f8537fa3448cd1f2c0bad14d
Miroslav Manchev <miroslav.manchev@accelya.com>
2020-12-01 14:41:53 +0200

    .

--- oauth2-social-login-start/oauth2-social-login-start--client/src/main/java/com/baeldung/lsso/spring/ClientSecurityConfig.java
+++ oauth2-social-login-start/oauth2-social-login-start--client/src/main/java/com/baeldung/lsso/spring/ClientSecurityConfig.java
@@ -1,12 +1,16 @@
 package com.baeldung.lsso.spring;
 
+import org.springframework.boot.web.codec.CodecCustomizer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.codec.LoggingCodecSupport;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @EnableWebSecurity
@@ -29,6 +33,8 @@ public class ClientSecurityConfig extends WebSecurityConfigurerAdapter {
         ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 = new ServletOAuth2AuthorizedClientExchangeFilterFunction(clientRegistrationRepository, authorizedClientRepository);
         oauth2.setDefaultOAuth2AuthorizedClient(true);
         return WebClient.builder()
+            // This is to log headers, for logging the body, see: https://stackoverflow.com/questions/46154994/how-to-log-spring-5-webclient-call/
+            .codecs(clientCodecConfigurer -> clientCodecConfigurer.defaultCodecs().enableLoggingRequestDetails(true))
             .apply(oauth2.oauth2Configuration())
             .build();
     }
